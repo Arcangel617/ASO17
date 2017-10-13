d023832ce0fe30fd689b34087ca02b91b67dd2fb
Arcangel Andres <artigue_arcangel@hotmail.com>
2017-10-13 19:23:14 -0300

    ref: se termina el ejercicio1

--- 1-directorios.sh
+++ 1-directorios.sh
@@ -14,4 +14,38 @@
 # estos directorios para luego finalizar.-
 
 #!/bin/bash
-echo "Hola mundo"
+
+if [ -d ./bin ]; then
+	echo "El directorio ya existe"
+else
+	echo "El directorio no existe"
+	echo "Creando "
+	mkdir ./bin
+fi
+
+if [ -d ./doc ]; then
+	echo "El directorio ya existe"
+else
+	echo "El directorio no existe"
+	echo "Creando "
+	mkdir ./doc
+fi
+
+if [ -d ./script ]; then
+	echo "El directorio ya existe"
+else
+	echo "El directorio no existe"
+	echo "Creando "
+	mkdir ./script
+fi
+
+ln -s /etc ./etc
+if [[ $# -eq 0 ]]; then
+	echo "Error: No arguments found"
+	echo "Usage: 3-ping.sh <IP-o-FQDN> <TIEMPO>"
+	exit
+fi
+ln -s /var/log ./log
+
+mv ./1-directorios.sh ./script 
+
